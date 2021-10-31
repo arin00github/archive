ee66d503b5d61d68ab40b9873c476f16cb5499b0
DESKTOP-G5CJPTS\USER <otter0104@innodep.com>
2021-10-31 17:54:31 +0900

    delete subfoler

--- backend/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const port = 4300;
-const app = require("./server/app");
-
-app.listen(port, () =>
-  console.log(`Connecting Server Successfully on ${port} port`)
-);
